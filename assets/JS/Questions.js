b2873f042fffeef1fcd312e296307b01f5459d08
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-19 20:19:12 +0000

    update the creation of quiz questions

---
+++ assets/JS/Questions.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "title": "One of these is not a way to declaer a variable in JavaScript",
+        "choices": ["let", "string", "var", "const"],
+        "answer": "string"
+    },
+
+    {
+        "title": "How do we comment in JavaScript?",
+        "choices": ["else if", "single-line", "multiple-line", "if else"],
+        "answer": "single-line and multiple-lines"
+    },
+
+    {
+        "title": "which of theses is not type of operator in JvaScript?",
+        "choices": ["Assignment operator", "Arithmetic operator", "Object", "Logical operator"],
+        "answer": "Object"
+    },
+
+
+    {
+        "title": "In JavaScript, how do we write arrow function?",
+        "choices": ["let myFunction = () =>", " let myfunction () =>", "my function () =>", "myFunction () =>"],
+        "answer": "let myFunction () =>"
+    },
+
+
+    {
+        "title": "In JavaScript, which of these is not what we use array for?",
+        "choices": ["To store boolean", "To store string", "To store value", "To store number"],
+        "answer": "To store value"
+    },
+
+
+    {
+        "title": "which of these is conditional keyword?",
+        "choices": ["while", "for", "for each", "if"],
+        "answer": "if"
+    },
+
+
+];
